9af36f6994813e4ca974dca860a6e90b7639f5fd
robneal <robneal28@gmail.com>
2017-06-28 17:03:55 -0400

    initail v2 commit

---
+++ js/main.js
@@ -0,0 +1,56 @@
+jQuery(document).ready(function($){
+	console.log("js is twerking");
+	//console.log("The whole world hates ie, Stop using it! -__-"); 
+
+	// Smooth Scroolling on all local anchor links 
+	$(function() {
+	  $('a[href*="#"]:not([href="#"])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	}); 
+
+
+	// Mobile/Ham burger Menu Transition 
+	function MobileMenu(){
+
+		this.siteHeader = $('.site-header'); // header tag
+		this.menuIcon = $(".site-header__menu-icon"); // ham menu
+		//this.menuContent = $('.site-header__menu-content'); // nothing
+		this.siteHeaderLinks = $('.site-header__main-menu__links'); // all links in header tag
+		this.siteHeaderLogo = $('.site-header__logo'); // all links in header tag
+
+		this.events = function(){
+			this.menuIcon.click(this.toggleTheMenu.bind(this)); 
+			this.siteHeaderLinks.click(this.clearMenu.bind(this)); 
+			this.siteHeaderLogo.click(this.clearMenu.bind(this)); 
+		}
+
+		this.toggleTheMenu = function(){
+			//this.menuContent.toggleClass('site-header__menu-content--is-visible');  
+			this.siteHeader.toggleClass('site-header--is-expanded'); 
+			this.menuIcon.toggleClass('site-header__menu-icon--close-x');
+		}
+
+		this.clearMenu = function(){
+			this.siteHeader.removeClass('site-header--is-expanded');
+			this.menuIcon.removeClass('site-header__menu-icon--close-x');
+		}
+		this.events(); 
+	}
+
+	var levelupMenu = new MobileMenu; 
+
+
+
+
+
+});
